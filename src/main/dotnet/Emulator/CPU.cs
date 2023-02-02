b091203d00b78b1150c3da1ed47aa67345b11bbe
Johan Lindfors <Johan.Lindfors@truesec.se>
2023-02-02 19:27:37 +0100

    Implementing instructions

--- src/main/dotnet/Emulator/CPU.cs
+++ src/main/dotnet/Emulator/CPU.cs
@@ -41,9 +41,76 @@ public class CPU
     public void EmulateCycle() 
     {
         int opcode = memory.GetOpcode(pc);
-        // Console.WriteLine(Utils.GetHex(opcode));
+        Console.WriteLine(Utils.GetHex(opcode));
 
-        // TODO: Implement chip 8 instruction set
+        int x = (opcode & 0x0F00) >> 8;
+        int y = (opcode & 0x00F0) >> 4;
+
+        switch(opcode) {
+ 
+            case 0x00E0: // Clears the screen
+                screen.Clear();
+                drawFlag = true;
+                pc += 2;
+                return;
+ 
+            case 0x00EE:
+                pc = stack.Pop();
+                pc += 2;
+                return;
+ 
+            default:
+                break;
+        }
+
+        switch(opcode & 0xF000) {
+ 
+            case 0x1000:
+                pc = opcode & 0x0FFF;
+                break;
+ 
+            case 0x2000:
+                stack.Push(pc);
+                pc = opcode & 0x0FFF;
+                break; 
+
+            case 0x3000:
+                if(register.Get(x) == (opcode & 0x00FF)) {
+                    pc += 4;
+                } else {
+                    pc += 2;
+                }
+                break;
+
+            case 0x4000:
+                if(register.Get(x) != (opcode & 0x00FF)) {
+                    pc += 4;
+                } else {
+                    pc += 2;
+                }
+                break;
+
+            case 0x5000:
+                if(register.Get(x) == register.Get(y)) {
+                    pc += 4;
+                } else {
+                    pc += 2;
+                }
+                break;
+
+            case 0x6000:
+                register.Set(x, opcode & 0x00FF);
+                pc += 2;
+                break;
+
+            case 0x7000:
+                register.Apply(x, () => ));
+                pc += 2;
+                break;
+
+            default:
+                break;
+        }
     }
 
     public int DelayTimer { get { return delayTimer; } set { delayTimer = value; } }
