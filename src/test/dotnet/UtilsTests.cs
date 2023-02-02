f48ea1a4c3f5d766d4da1509f7126d8284f69bee
Johan Lindfors <Johan.Lindfors@truesec.se>
2023-02-02 08:08:01 +0100

    Accurate register test2

--- src/test/dotnet/UtilsTests.cs
+++ src/test/dotnet/UtilsTests.cs
@@ -6,8 +6,11 @@ public class UtilsTest
     public void RegisterApplyTest()
     {
         var register = new Chip8.Register();
+        register.Set(2, 4);
         int x = 0;
-        int y = 0;
+        int y = 2;
         register.Apply(x, vx => vx | register.Get(y));
+
+        Assert.Equal(4, register.Get(0));
     }
 }
